39699008d124be227e73139d753c0a01ff2221b6
MadhavTummala <madhavtummala@icloud.com>
2019-10-11 12:51:45 +0530

    Added assignment 10

--- 10/run.sh
+++ 10/run.sh
@@ -1,15 +1,5 @@
-if [ "$1" == "debug" ]
-then
-	lex lex.l && gcc lex.yy.c && ./a.out
-	lex assign7.l
-	yacc assign7.y -d -v
-	gcc y.tab.c -DYYDEBUG=1
-else
-	lex assign7.l
-	yacc assign7.y -d
-	gcc y.tab.c
-fi
+lex assign10.l
+yacc assign10.y
+cc y.tab.c -ly -ll -lm
 
 ./a.out
-
-rm a.out lex.yy.c y.tab.c y.tab.h
